32f214dac7752d768626428eac888d1e6ca02453
Michael Dekker <git@michaeldekker.nl>
2016-05-27 19:03:23 +0200

    Change license - Cleanup

--- mdstripe.php
+++ mdstripe.php
@@ -1,28 +1,21 @@
 <?php
 /**
-* 2007-2015 PrestaShop
-*
-* NOTICE OF LICENSE
-*
-* This source file is subject to the Academic Free License (AFL 3.0)
-* that is bundled with this package in the file LICENSE.txt.
-* It is also available through the world-wide-web at this URL:
-* http://opensource.org/licenses/afl-3.0.php
-* If you did not receive a copy of the license and are unable to
-* obtain it through the world-wide-web, please send an email
-* to license@prestashop.com so we can send you a copy immediately.
-*
-* DISCLAIMER
-*
-* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
-* versions in the future. If you wish to customize PrestaShop for your
-* needs please refer to http://www.prestashop.com for more information.
-*
-*  @author    PrestaShop SA <contact@prestashop.com>
-*  @copyright 2007-2015 PrestaShop SA
-*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
-*  International Registered Trademark & Property of PrestaShop SA
-*/
+ * 2016 Michael Dekker
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@michaeldekker.com so we can send you a copy immediately.
+ *
+ *  @author    Michael Dekker <prestashop@michaeldekker.com>
+ *  @copyright 2016 Michael Dekker
+ *  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ */
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -325,38 +318,11 @@ class MDStripe extends PaymentModule
     }
 
     /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    */
-    public function hookBackOfficeHeader()
-    {
-        if (Tools::getValue('module_name') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
-        }
-    }
-
-    /**
-     * Add the CSS & JavaScript files you want to be added on the FO.
-     */
-    public function hookHeader()
-    {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
-    }
-
-    /**
      * This method is used to render the payment button,
      * Take care if the button should be displayed or not.
      */
     public function hookPayment($params)
     {
-//        $currency_id = $params['cart']->id_currency;
-//        $currency = new Currency((int)$currency_id);
-//
-//        if (in_array($currency->iso_code, $this->limited_currencies) == false) {
-//            return false;
-//        }
-
         /** @var Cookie $email */
         $cookie = $params['cookie'];
         $stripe_email = $cookie->email;
