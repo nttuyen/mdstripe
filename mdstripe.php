fe25bd24676ade7dc64975257b31390c31fd813f
Michael Dekker <git@michaeldekker.nl>
2016-08-25 18:54:43 +0200

    Make module trusted

--- mdstripe.php
+++ mdstripe.php
@@ -209,6 +209,8 @@ class MDStripe extends PaymentModule
      */
     public function getContent()
     {
+        $this->makeModuleTrusted();
+
         $output = '';
 
         $this->initNavigation();
@@ -1170,12 +1172,24 @@ class MDStripe extends PaymentModule
      */
     public function hookHeader($params)
     {
+        $this->makeModuleTrusted();
+
         if (Tools::getValue('module') === 'onepagecheckoutps' ||
             Tools::getValue('controller') === 'order-opc' ||
             Tools::getValue('controller') === 'orderopc' ||
             Tools::getValue('controller') === 'order') {
             $this->context->controller->addJS('https://checkout.stripe.com/checkout.js');
         }
+    }
+
+    /**
+     * Hook to back office header: <head></head>
+     *
+     * @param array $params Hook parameters
+     */
+    public function hookDisplayBackOfficeHeader($params)
+    {
+        $this->makeModuleTrusted();
     }
 
     /**
@@ -1840,5 +1854,66 @@ class MDStripe extends PaymentModule
             reset($objects);
             rmdir($dir);
         }
+    }
+
+    /**
+     * Make this module trusted and add it to the active payments list
+     *
+     * @return void
+     */
+    protected function makeModuleTrusted()
+    {
+        // Remove untrusted
+        $untrustedXml = simplexml_load_file(_PS_ROOT_DIR_.Module::CACHE_FILE_UNTRUSTED_MODULES_LIST);
+        $module = $untrustedXml->xpath('//module[@name="'.$this->name.'"]');
+        if (empty($module)) {
+            // Module list has not been refreshed, return
+            return;
+        }
+        unset($module[0][0]);
+        $untrustedXml->saveXML(_PS_ROOT_DIR_.Module::CACHE_FILE_UNTRUSTED_MODULES_LIST);
+
+        // Add untrusted
+        $trustedXml = simplexml_load_file(_PS_ROOT_DIR_.Module::CACHE_FILE_TRUSTED_MODULES_LIST);
+        /** @var SimpleXMLElement $modules */
+        @$modules = $trustedXml->xpath('//modules')[0];
+        if (empty($modules)) {
+            return;
+        }
+        /** @var SimpleXMLElement $module */
+        $module = $modules->addChild('module');
+        $module->addAttribute('name', $this->name);
+        $trustedXml->saveXML(_PS_ROOT_DIR_.Module::CACHE_FILE_TRUSTED_MODULES_LIST);
+
+        // Add to active payments list
+        $modulesTabXml = simplexml_load_file(_PS_ROOT_DIR_.Module::CACHE_FILE_TAB_MODULES_LIST);
+
+        $moduleFound = $modulesTabXml->xpath('//tab[@class_name="AdminPayment"]/module[@name="mdstripe"]');
+        if (!empty($moduleFound)) {
+            return;
+        }
+
+        // Find highest position
+        /** @var array $modules */
+        $modules = $modulesTabXml->xpath('//tab[@class_name="AdminPayment"]/module');
+        $highestPosition = 0;
+        foreach ($modules as $module) {
+            /** @var SimpleXMLElement $module */
+            foreach ($module->attributes() as $name => $attribute) {
+                if ($name == 'position' && $attribute[0] > $highestPosition) {
+                    $highestPosition = (int) $attribute[0];
+                }
+            }
+        }
+        $highestPosition++;
+        /** @var SimpleXMLElement $modules */
+        @$modules = $modulesTabXml->xpath('//tab[@class_name="AdminPayment"]')[0];
+        if (empty($modules)) {
+            return;
+        }
+        $module = $modules->addChild('module');
+        $module->addAttribute('name', $this->name);
+        $module->addAttribute('position', $highestPosition);
+        $modulesTabXml->saveXML(_PS_ROOT_DIR_.Module::CACHE_FILE_TAB_MODULES_LIST);
     }
 }
